50dc3a8c12f779d7eea61f29e4729463a4d18ef9
root <root@gsuresh071c.mylabserver.com>
2020-05-27 22:47:15 +0000

    added Vhosts defined type

---
+++ manifests/vhosts.pp
@@ -0,0 +1,25 @@
+# Generates virtual hosts file
+#
+# @example
+#   apache::vhossts { 'namevar': }
+define apache::vhosts (
+  Integer   $port,
+  String[1] $subdomain,
+  String    $admin,
+  String    $docroot,
+) {
+  file { "${docroot}":
+    ensure => 'directory',
+    owner  => $apache::vhosts_owner,
+    group  => $apache::vhosts_group,
+  }
+
+  file { "${apache::vhosts_dir}/${subdomain}.conf":
+    ensure    => 'file',
+    owner     => $apache::vhosts_owner,
+    group     => $apache::vhosts_group,
+    mode      => '0644',
+    content   => epp('apache/vhosts.conf.epp', {'port' => $port, 'subdomain' => $subdomain, 'admin' => $admin, 'docroot' => $docroot}),
+    notify    => Service['apache_service'],
+  }
+}
